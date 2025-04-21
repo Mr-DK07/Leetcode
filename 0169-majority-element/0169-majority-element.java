1326abc868dcc82bccecc1348c3f10071a1a2275
D K <143409270+Mr-DK07@users.noreply.github.com>
2025-04-21 15:02:51 +0530

    Time: 3 ms (56.57%), Space: 53.4 MB (29.7%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,12 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        int ans = 0;
         Arrays.sort(nums);
-
-        if (n > 0) {
-            ans = nums[n / 2];
-        }
-        return ans;
+        return nums[nums.length / 2];
     }
 }
